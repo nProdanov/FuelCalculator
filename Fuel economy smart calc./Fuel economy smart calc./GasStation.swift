5ca0189b36395658840cf6d218d886fb25fa923e
Nikolay Prodanow <nikolayprodanow@gmail.com>
2017-03-27 14:27:53 +0300

    add new model for gas station

---
+++ Fuel economy smart calc./Fuel economy smart calc./GasStation.swift
@@ -0,0 +1,19 @@
+//
+//  GasStation.swift
+//  Fuel economy smart calc.
+//
+//  Created by Nikolay Prodanow on 3/27/17.
+//  Copyright © 2017 Nikolay Prodanow. All rights reserved.
+//
+
+import Foundation
+
+struct GasStation {
+    var address: String
+    var brandname: String
+    var city: String
+    var id: Int
+    var latitude: Double
+    var longtitude: Double
+    var name: String
+}
